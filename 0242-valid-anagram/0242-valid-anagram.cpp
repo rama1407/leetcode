ba2b3ae6f007e77e838d5a009fd3298b0fb32f84
rama1407 <ramalakshmirongali2005@gmail.com>
2025-10-23 18:40:08 +0530

    Time: 4 ms (38.47%), Space: 9.6 MB (91.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
         map<char,int> a;
         map<char,int> b;
         for(char c: s)
