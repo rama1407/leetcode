75ee34bdc340a1c6c5ce46622498c73c502a81eb
rama1407 <ramalakshmirongali2005@gmail.com>
2026-02-03 08:07:08 +0530

    Time: 0 ms (100%), Space: 29.5 MB (91.1%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,15 +4,8 @@ public:
         int n = nums.size();
         k = k % n;           
         if (k == 0) return;
-        vector<int> temp;
-        for (int i = 0; i < n - k; i++) {
-            temp.push_back(nums[i]);
-        }
-        for (int j = 0; j < k; j++) {
-            nums[j] = nums[j + (n - k)];
-        }
-        for (int x = k; x < n; x++) {
-            nums[x] = temp[x - k];
-        }
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
