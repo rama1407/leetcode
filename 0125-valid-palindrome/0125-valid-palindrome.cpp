55938472958f384b11ed84b85dd2ca6dfd51b0ff
rama1407 <ramalakshmirongali2005@gmail.com>
2026-03-22 16:07:07 +0530

    Time: 0 ms (100%), Space: 9.9 MB (53.74%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string p;
-        for(char c: s){
-           if(isalnum(c)){
-            p+=tolower(c);
-           }
-        }
         int l = 0;
-        int r = p.size()-1;
+        int r = s.size()-1;
         bool found = true;
         while(l<=r){
-            if(p[l]!=p[r]) {
-               found = false;
-               break;
+            while(l<r&&!isalnum(s[l])) l++;
+            while(l<r&&!isalnum(s[r])) r--;
+           if(tolower(s[l])!=tolower(s[r])){
+                return false;
             }
             l++;
             r--;
         }
-        if(found) return true;
-        return false;
+        return true;
     }
 };
