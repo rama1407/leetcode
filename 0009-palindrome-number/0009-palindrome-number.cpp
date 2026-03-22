fc634b40506321fd429d9f64a320d1b573203bda
rama1407 <ramalakshmirongali2005@gmail.com>
2026-03-22 15:03:59 +0530

    Time: 0 ms (100%), Space: 8.5 MB (65.09%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0)  return false;
-        long long reversedigit = 0;
-        int original = x;
-        while(x!=0)
-        {
+       if(x<0) return false;
+       int original = x;
+       long long reversednumber = 0;
+       while(x>0){
         int digit = x%10;
-        x /= 10;
-        reversedigit = reversedigit*10 + digit;
-        }
-        if(original == reversedigit) return true;
+        reversednumber = reversednumber*10+digit;
+        x = x/10;
+       }
+       if(original == reversednumber) return true;
         return false;
     }
 };
