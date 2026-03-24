d8a335032798beadbd366514b07ef43d3afa9cf7
rama1407 <ramalakshmirongali2005@gmail.com>
2026-03-25 00:57:58 +0530

    Time: 8 ms (72.61%), Space: 9.4 MB (79.94%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    bool ispal(string &s,int l,int r){
-        while(l<r){
-            if(s[l]!=s[r]) return false;
-            l++;
-            r--;
-        }
-        return true;
-    }
     string longestPalindrome(string s) {
-        int maxcount = 0;
-        int start = 0;
         int n = s.size();
+        int maxlen = 0;
+        int start = 0;
         for(int i = 0;i<n;i++){
-            for(int j = i;j<n;j++){
-               if(ispal(s,i,j)){
-                  if(j-i+1>maxcount){
-                     maxcount = j-i+1;
-                     start = i;
-                  }
-               }
+            int l = i;
+            int r = i;
+            while(l>=0&&r<n&&s[l]==s[r]){
+                if(r-l+1>maxlen){
+                    maxlen = r-l+1;
+                    start = l;
+                }
+                l--;
+                r++;
+            }
+            int l1 = i;
+            int r1 = i+1;
+            while(l1>=0&&r1<n&&s[l1]==s[r1]){
+                if(r1-l1+1>maxlen){
+                    maxlen = r1-l1+1;
+                    start = l1;
+                }
+                l1--;
+                r1++;
             }
         }
-        return s.substr(start,maxcount);
+      return s.substr(start,maxlen);
     }
 };
