6915c166e1012b87facfa7c3a0cfade36a55d3a0
rama1407 <ramalakshmirongali2005@gmail.com>
2026-07-01 20:09:42 +0530

    Time: 0 ms (100%), Space: 8.5 MB (54.78%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,30 +1,11 @@
 class Solution {
 public:
     string makeGood(string s) {
-        int n = s.size();
-        stack<int> st;
-        for(int i = 0;i<n;i++){
-            if(!st.empty()){
-                if(isupper(s[i])){
-                    if(st.top()==tolower(s[i])) st.pop();
-                    else st.push(s[i]);
-                }
-                else if(islower(s[i])){
-                    if(st.top()==toupper(s[i])) st.pop();
-                    else st.push(s[i]);
-                }
-            } 
-            else st.push(s[i]);
+        string ans;
+        for(char c : s){
+            if(!ans.empty()&& abs(ans.back()-c)==32 ) ans.pop_back();
+            else ans.push_back(c);
         }
-        int x = st.size();
-        string rev;
-        while(x){
-            int c = st.top();
-            rev+= c;
-            st.pop();
-            x--;
-        }
-        reverse(rev.begin(),rev.end());
-        return rev;
+        return ans;
     }
 };
