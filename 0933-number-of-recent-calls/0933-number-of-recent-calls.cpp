d36ff046147bc8df88a568e3426509693dce441a
rama1407 <ramalakshmirongali2005@gmail.com>
2026-07-02 16:49:30 +0530

    Time: 18 ms (50.37%), Space: 64.2 MB (67.35%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
@@ -5,25 +5,9 @@ public:
     }
     queue<int> q;
     int ping(int t) {
-        int count = 0;
-        int arr[2];
-        arr[0] = t-3000;
-        arr[1] = t;
         q.push(t);
-        int n = q.size();
-        for(int i = 0;i<n;i++){
-            if(q.front()>=arr[0] && q.front()<=arr[1]){
-                count++;
-                int x = q.front();
-                q.pop();
-                q.push(x);
-            }
-            else {int x = q.front();
-            q.pop();
-            q.push(x);
-            }
-        }
-        return count;
+        while(!q.empty() && q.front()<t-3000) q.pop();
+        return q.size();
     }
 };
 
