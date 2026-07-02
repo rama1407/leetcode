0de6c430f8718afb6db2aeb43ce79974c0acc84d
rama1407 <ramalakshmirongali2005@gmail.com>
2026-07-02 21:20:04 +0530

    Time: 19 ms (5.05%), Space: 12.4 MB (82.77%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,21 +3,36 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int x = nums1.size();
         int y =  nums2.size();
+        int arr[y];
+        stack<int> st;
+        for(int i = y-1;i>=0;i--){
+             if(st.empty()){
+                arr[i]=-1;
+                st.push(nums2[i]);
+             }
+             else {
+                if(nums2[i]<st.top()){
+                    arr[i]=st.top();
+                    st.push(nums2[i]);
+                }
+                else {
+                    while(!st.empty() && st.top() <= nums2[i]) st.pop();
+                    if(st.empty()) {
+                        arr[i] = -1;
+                        st.push(nums2[i]);
+                    }
+                    else {
+                        arr[i] = st.top();
+                        st.push(nums2[i]);
+                    }
+                }
+             }
+        }
         vector<int> ans;
         for(int i = 0;i<x;i++){
-            for(int j = 0;j<y;j++){
-                if(nums1[i]==nums2[j]){
-                    bool found = false;
-                    for(int k = j+1;k<y;k++){
-                        if(nums2[k]>nums2[j]) {
-                            ans.push_back(nums2[k]);
-                            found = true;
-                            break;
-                        }
-                    }
-                    if(!found) ans.push_back(-1);
-                }
-            }
+             for(int j = 0;j<y;j++){
+                if(nums1[i]==nums2[j]) ans.push_back(arr[j]);
+             }
         }
         return ans;
     }
