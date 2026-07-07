e6f3c3cf2a96efc9e43606ea3886994d1ae0115a
rama1407 <ramalakshmirongali2005@gmail.com>
2026-07-08 01:53:19 +0530

    Time: 0 ms (100%), Space: 15.1 MB (59.6%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        int count = 0;
+        for(int i= 0;i<n;i++){
+            for(int j =0;j<n;j++){
+                if(j==i) continue;
+                else if(intervals[i][0]>=intervals[j][0] && intervals[i][1]<=intervals[j][1]) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return n-count;
+    }
+};
