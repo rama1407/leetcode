e2f6e9977d2cf6d28c1064f5dd43177b94271cba
rama1407 <ramalakshmirongali2005@gmail.com>
2026-09-23 11:50:56 +0530

    Time: 1 ms (7.21%), Space: 9.9 MB (50.78%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        vector<int> a = stones;
-        int n = a.size();
-        if(n==1) return stones[0];
-        if(n==2){
-            if(a[0]<a[1]) return a[1]-a[0];
-            else if(a[0] > a[1]) return a[0]-a[1];
-            else return 0;
+        multiset<int> a;
+        for(int it: stones){
+            a.insert(it);
         }
-        bool found = true;
-        while(found){
-            sort(a.begin(),a.end());
-            if(a[n-2]==a[n-1]) {
-                n = n-2;
-            }
-            else if(a[n-2]<a[n-1]) {
-                a[n-2] = a[n-1] - a[n-2];
-                n = n-1;
-            }
-            if(n==0 || n==1) {
-                found = false;
-            }
+        while(a.size()>=2){
+            int val1 = *a.rbegin();
+            a.erase(prev(a.end()));
+            int val2 = *a.rbegin();
+            a.erase(prev(a.end()));
+            if(val2<val1) a.insert(val1 - val2);   
         }
-        if(n==1) return a[0];
+        if(a.size()==1) return *a.rbegin();
         else return 0;
     }
 };
