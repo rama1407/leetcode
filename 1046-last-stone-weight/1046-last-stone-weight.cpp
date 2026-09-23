df69a4a068031506a57d8a9528a33f14080e4f26
rama1407 <ramalakshmirongali2005@gmail.com>
2026-09-23 12:07:05 +0530

    Time: 0 ms (100%), Space: 10 MB (50.78%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        multiset<int> a;
+        priority_queue<int> pq;
         for(int it: stones){
-            a.insert(it);
+            pq.push(it);
         }
-        while(a.size()>=2){
-            int val1 = *a.rbegin();
-            a.erase(prev(a.end()));
-            int val2 = *a.rbegin();
-            a.erase(prev(a.end()));
-            if(val2<val1) a.insert(val1 - val2);   
+        while(pq.size()>=2){
+            int val1 = pq.top();
+            pq.pop();
+            int val2 = pq.top();
+            pq.pop();
+            if(val2<val1){
+                pq.push(val1-val2);
+            }
         }
-        if(a.size()==1) return *a.rbegin();
-        else return 0;
+        return pq.empty()?0:pq.top();
     }
 };
